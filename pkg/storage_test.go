3af3b2719103a12011f2590e43e4d831b63b6a2c
8ugr4 <karadumanbugra@hotmail.com>
2026-02-05 22:57:14 +0100

    test: add test for exif funcs, add evil_gopher for testing purposes
    disable check-added-large-files for this commit, rollback in next commit

--- pkg/storage_test.go
+++ pkg/storage_test.go
@@ -1,7 +1,9 @@
 package pkg
 
 import (
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
 
@@ -14,11 +16,28 @@ func Test_initExifTool(t *testing.T) {
 	require.NotNil(t, o.Storage.Exif)
 }
 
-//func Test_getFileDate(t *testing.T) {
-//	o := GetNewOperator()
-//	fp := "../testDir/dummy2.jpg"
-//	regexPattern := "*"
-//	periodType := "year"
-//	res, err := o.getFileDate(fp, regexPattern, periodType)
-//	fmt.Println(res, err)
-//}
+func Test_getFileDate(t *testing.T) {
+	o := GetNewOperator()
+	err := o.initExifTool()
+	require.NoError(t, err)
+
+	// injected a date (for testing purposes) into this file with following call:
+	// exiftool -CreateDate="2022:12:08 19:09:53" pkg/evil_gopher.png
+	// evil_gopher.png belongs to https://github.com/MariaLetta/free-gophers-pack/blob/master/characters/png/1.png
+	{
+		res, err := o.getFileDate("./evil_gopher.png", "month")
+		require.NoError(t, err)
+		assert.Equal(t, "2022-12", res)
+	}
+	{
+		res, err := o.getFileDate("./evil_gopher.png", "year")
+		require.NoError(t, err)
+		assert.Equal(t, "2022", res)
+	}
+
+	f, err := os.Create("random.png")
+	require.NoError(t, err)
+	_, err = o.getFileDate(f.Name(), "year")
+	require.Equal(t, err, ErrorNoCreateDate)
+	require.NoError(t, os.Remove(f.Name()))
+}
