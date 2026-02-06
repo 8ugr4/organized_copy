c177438b7131026469a3a6e9c35d29b521acbdc8
8ugr4 <karadumanbugra@hotmail.com>
2026-02-07 00:06:07 +0100

    refactor: move exif logic to it's own file

---
+++ pkg/exif.go
@@ -0,0 +1,73 @@
+package pkg
+
+import (
+	"errors"
+	"fmt"
+	"github.com/barasher/go-exiftool"
+	"os"
+	"time"
+)
+
+var ErrorNoCreateDate = errors.New("given file doesn't have a CreateDate field or we failed to find it")
+
+func initExifTool() (*exiftool.Exiftool, error) {
+	exifTool, err := exiftool.NewExiftool()
+	if err != nil {
+		return nil, err
+	}
+	return exifTool, nil
+}
+
+// getFileDate tries EXIF -> ModTime -> regex from filename and returns either month or year as string
+// periodType is "month" or "year"
+// TODO: next steps are:
+//  1. if  `sort: "year || month"` is defined in a `category` in `rules.yaml` file, then call this func with sort value.
+//  2. hold a years || months map, (so unique vals only), when getting returns from this func, and then mkdir() those keys.
+func (o *Operator) getFileDate(fp, periodType string) (string, error) { //nolint:unused
+	f, err := os.Open(fp)
+	if err != nil {
+		return "", err
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(f)
+
+	var timePeriod string
+	fileInfos := o.Storage.Exif.ExtractMetadata(f.Name())
+	for _, fileInfo := range fileInfos {
+		if fileInfo.Err != nil {
+			return "", fileInfo.Err
+		}
+		if date, exists := fileInfo.Fields["CreateDate"]; exists {
+			timePeriod = date.(string)
+		}
+	}
+
+	if timePeriod != "" {
+		parseTime, err := func(timePeriod, periodType string) (string, error) {
+			ta, timeError := time.Parse("2006:01:02 15:04:05", timePeriod)
+			if timeError != nil {
+				return "", timeError
+			}
+			switch periodType {
+			case "month":
+				return ta.Format("2006-01"), nil
+			case "year":
+				return ta.Format("2006"), nil
+			default:
+				return "", errors.New("no time thingy m8")
+			}
+		}(timePeriod, periodType)
+		if err != nil {
+			return "", err
+		}
+		if parseTime == "" {
+			return "", fmt.Errorf("invalid periodType %s, must be 'month' or 'year'", periodType)
+		}
+		return parseTime, nil
+	}
+	return "", ErrorNoCreateDate
+}
